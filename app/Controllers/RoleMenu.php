a10238ac15b4f6bc84e796b6bb397e7bf1353abe
Alghi <alghi@MacBook-Air-Alghi.local>
2024-12-14 21:58:13 +0700

    ref : refractoring code access control list

--- app/Controllers/RoleMenu.php
+++ app/Controllers/RoleMenu.php
@@ -3,75 +3,82 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use App\Models\MenuModel;
-use App\Models\RoleMenuModel;
-use App\Models\RoleModel;
+use App\Models\{
+    MenuModel,
+    RoleModel,
+    RoleMenuModel
+};
 
 class RoleMenu extends BaseController
 {
+    protected  $__role_model,$__menu_model,$__menu_has_role;
+
+    public function __construct() {
+        $this->__role_model    = new RoleModel();
+        $this->__menu_model    = new MenuModel();
+        $this->__menu_has_role = new RoleMenuModel();
+    }
+
     public function index()
     {
-        $redirect = $this->checkRole();
-        if ($redirect) return $redirect;
+        return $this->checkRole(function() {
+            $roles = $this->__role_model->findAll();
+            $menus = $this->__menu_model->findAll();
 
-        $roleModel = new RoleModel();
-        $roles = $roleModel->findAll();
+            $roleAccess = [];
+            foreach ($roles as $role) {
+                $menuAccess = $this->__menu_has_role->getMenusByRole($role['id']);
+                $roleAccess[$role['id']] = array_column($menuAccess, 'id');
+            }
+            return view('role_access_view', compact('roles', 'menus', 'roleAccess'));
+        });
 
-        $menuModel = new MenuModel();
-        $menus = $menuModel->findAll();
-
-        $roleMenuModel = new RoleMenuModel();
-        $roleAccess = [];
-        foreach ($roles as $role) {
-            $menuAccess = $roleMenuModel->getMenusByRole($role['id']);
-            $roleAccess[$role['id']] = array_column($menuAccess, 'id');
-        }
-        return view('role_access_view', compact('roles', 'menus', 'roleAccess'));
     }
 
     public function update()
     {
-        $redirect = $this->checkRole();
-        if ($redirect) return $redirect;
+        return $this->checkRole(function () {
+            $data = $this->request->getPost();
+            unset($data['csrf_test_name']);
 
-        $data = $this->request->getPost();
-        unset($data['csrf_test_name']);
+            foreach ($data as $roleName => $arrayMenuId) {
+                $role      = $this->__role_model->where('nama', $roleName)->first();
 
-        foreach ($data as $roleName => $arrayMenuId) {
-            $roleModel = new RoleModel();
-            $role = $roleModel->where('nama', $roleName)->first();
-            if ($role) {
-                $roleId = $role['id'];
-                foreach ($arrayMenuId as $menuId) {
-                    $roleMenuModel = new RoleMenuModel();
-                    $isExists = $roleMenuModel->where('role_id', $roleId)->where('menu_id', $menuId)->get()->getRow();
+                if ($role) {
+                    $roleId = $role['id'];
 
-                    if (!$isExists) {
-                        $roleMenuModel->insert([
-                            'role_id' => $roleId,
-                            'menu_id' => $menuId
-                        ]);
+                    foreach ($arrayMenuId as $menuId) {
+                        $isExists  = $this->__menu_has_role->where('role_id', $roleId)->where('menu_id', $menuId)->get()->getRow();
+
+                        if (!$isExists) {
+                            $this->__menu_has_role->insert([
+                                'role_id' => $roleId,
+                                'menu_id' => $menuId
+                            ]);
+                        }
                     }
+
+                    $this->__menu_has_role->where('role_id', $roleId)->whereNotIn('menu_id', $arrayMenuId)->delete();
                 }
-
-                $roleMenuModel->where('role_id', $roleId)->whereNotIn('menu_id', $arrayMenuId)->delete();
             }
-        }
 
-        session()->setFlashdata('success', 'Berhasil menyimpan perubahan data role access');
-        return redirect()->to('role-access');
+            session()->setFlashdata('success', 'Berhasil menyimpan perubahan data role access');
+            return redirect()->to('role-access');
+        });
+
     }
 
-    function checkRole()
+    function checkRole(callable $callback)
     {
-        $roleId = session()->get('role_id');
-        $roleModel = new RoleModel();
-        $superadmin = $roleModel->where('nama', 'superadmin')->first();
+        $roleId       = session()->get('role_id');
+        $superadmin   = $this->__role_model->where('nama', 'superadmin')->first();
         $superadminId = $superadmin ? $superadmin['id'] : 0;
 
         if ($roleId != $superadminId) {
             session()->setFlashdata('notice', 'Anda tidak memiliki akses ke halaman ini.');
             return redirect()->to('dashboard');
         }
+
+        return $callback();
     }
 }
