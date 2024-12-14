154c722d484cc1adb64b758c477a0127eb89b2ab
Alghi <alghi@MacBook-Air-Alghi.local>
2024-12-14 23:43:22 +0700

    fix : undo middleware auth

--- app/Filters/UsersAuthFilter.php
+++ app/Filters/UsersAuthFilter.php
@@ -13,7 +13,6 @@ class UsersAuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        return "alghi";
         if(is_null(session()->get('logged_in')))
         {
             return redirect()->to(base_url('login/index'));
