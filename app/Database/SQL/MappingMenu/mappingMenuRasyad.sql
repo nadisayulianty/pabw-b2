f32cff8f3c42c965591a0928752ada86b813f4c2
rasyadsr <mchmdrasyad@gmail.com>
2024-12-21 17:38:05 +0700

    feat(rasyad): ganti nama menu

--- app/Database/SQL/MappingMenu/mappingMenuRasyad.sql
+++ app/Database/SQL/MappingMenu/mappingMenuRasyad.sql
@@ -1,5 +1,5 @@
 INSERT INTO menus (nama, route, created_at)
 VALUES
     ('Table wilayah', '/table/wilayah', NOW()),
-    ('Table List aktivitas mahasiswa', '/table/list-aktivitas-mahasiswa', NOW()),
+    ('Table jenis aktivitas mahasiswa', '/table/jenis-aktivitas-mahasiswa', NOW()),
     ('List jenis keluar', '/table/jenis-keluar', NOW());
